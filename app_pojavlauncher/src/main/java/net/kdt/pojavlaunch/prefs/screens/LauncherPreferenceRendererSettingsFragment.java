ea43e8ad84e5d433814248557515aebaa027cd37
alexytomi <60690056+alexytomi@users.noreply.github.com>
2025-07-19 23:20:54 +0800

    update(MobileGlues): Update to 1.2.7 release
    Added new options
    - angleDepthClearFixMode
    - timerQueryExt
    - Compute multidraw emulation

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceRendererSettingsFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceRendererSettingsFragment.java
@@ -7,10 +7,10 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 
-import androidx.annotation.NonNull;
 import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
 
 import net.kdt.pojavlaunch.R;
 
@@ -18,10 +18,16 @@ import java.util.Objects;
 
 public class LauncherPreferenceRendererSettingsFragment extends LauncherPreferenceFragment {
     EditTextPreference GLSLCachePreference;
+    ListPreference MultiDrawEmulationPreference;
+    SwitchPreference ComputeMultiDrawPreference;
+    Preference.SummaryProvider MultiDrawSummaryProvider;
+
     @Override
     public void onCreatePreferences(Bundle b, String str) {
         addPreferencesFromResource(R.xml.pref_renderer);
         GLSLCachePreference = findPreference("mg_renderer_setting_glsl_cache_size");
+        ComputeMultiDrawPreference = findPreference("mg_renderer_multidrawCompute");
+        MultiDrawEmulationPreference = findPreference("mg_renderer_setting_multidraw");
         GLSLCachePreference.setOnBindEditTextListener((editText) -> {
             editText.setInputType(TYPE_CLASS_NUMBER);
             editText.addTextChangedListener(new TextWatcher() {
@@ -29,18 +35,20 @@ public class LauncherPreferenceRendererSettingsFragment extends LauncherPreferen
                 public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                     // Nothing, its boilerplate
                 }
+
                 @Override
                 public void afterTextChanged(Editable editable) {
                     // Nothing, its boilerplate
                 }
+
                 @Override
                 public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                     // This is just to handle the summary not updating when its above max int value
                     // Horrible I know.
-                    if (editText.getText().toString().isEmpty()){
+                    if (editText.getText().toString().isEmpty()) {
                         editText.setText("0");
                     }
-                    if (Long.parseLong(editText.getText().toString()) > Integer.MAX_VALUE){
+                    if (Long.parseLong(editText.getText().toString()) > Integer.MAX_VALUE) {
                         editText.setError("Too big! Setting to maximum value");
                         editText.setText(String.valueOf(Integer.MAX_VALUE));
                     }
@@ -49,12 +57,30 @@ public class LauncherPreferenceRendererSettingsFragment extends LauncherPreferen
             });
         });
         updateGLSLCacheSummary(); // Just updates the summary with the value when user opens the menu. Yes it's out of place.
+        updateMultiDrawSummary(); // Same as above
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences p, String s) {
         GLSLCachePreference = findPreference("mg_renderer_setting_glsl_cache_size");
         updateGLSLCacheSummary();
+        updateMultiDrawSummary();
+    }
+
+    private void updateMultiDrawSummary() {
+        if (MultiDrawEmulationPreference != null) {
+            if (MultiDrawEmulationPreference.getSummaryProvider() != null) {
+                MultiDrawSummaryProvider = MultiDrawEmulationPreference.getSummaryProvider();
+            }
+            if (ComputeMultiDrawPreference.isChecked()) {
+                MultiDrawEmulationPreference.setEnabled(false);
+                MultiDrawEmulationPreference.setSummaryProvider(null);
+                MultiDrawEmulationPreference.setSummary("(Experimental) Compute");
+            } else if (MultiDrawEmulationPreference != null) {
+                MultiDrawEmulationPreference.setEnabled(true);
+                MultiDrawEmulationPreference.setSummaryProvider(MultiDrawSummaryProvider);
+            }
+        }
     }
 
     private void updateGLSLCacheSummary() {
@@ -62,6 +88,8 @@ public class LauncherPreferenceRendererSettingsFragment extends LauncherPreferen
             if (Objects.equals(Objects.requireNonNull(this.GLSLCachePreference).getText(), "") || Integer.parseInt(Objects.requireNonNull(this.GLSLCachePreference.getText())) == 0) {
                 this.GLSLCachePreference.setSummary(getString(R.string.global_off));
             } else this.GLSLCachePreference.setSummary(this.GLSLCachePreference.getText() + " MB");
-        } catch (Exception e){ e.printStackTrace(); }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
